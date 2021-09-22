a79e5545f5e18d51fd349353ad15922ffde49505
kawsarm104 <kawsarm104@gmail.com>
2021-09-22 10:11:58 +0600

    little change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Nav from "./components/navbar/Nav";
 import RestCountry from "./components/restcountry/RestCountry";
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <RestCountry/>
     </div>
   );
